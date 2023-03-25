6b14d069734f499dc5d67391136f888560f2408c
Divyansh Jindal <ninjsa@hotmail.com>
2023-03-25 22:59:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -106,7 +106,7 @@ async def send_doc(client, message):
             await message.reply_text("**__You are not subscribed my channel__** ",
                                      reply_to_message_id=message.id,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/jindalTechX"{update_channel}")]]))
+                                         [[InlineKeyboardButton("🔺 Update Channel 🔺", url=f"https://t.me/{update_channel}")]]))
             await client.send_message(log_channel,f"🦋 #PiracyOff_LOGS 🦋,\n\n**ID** : `{user_id}`\n**Name**: {message.from_user.first_name} {message.from_user.last_name}\n**User-Plan** : {user}\n\n ",
                                                                                                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Restrict User ( **pm** ) 🔺", callback_data="ceasepower")]]))
             return
